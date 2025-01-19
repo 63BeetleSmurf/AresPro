464e56c8c83a7b5ed57d3975fa60907631a78269
63BeetleSmurf <scott@murphys.place>
2025-01-19 10:05:56 +0000

    Update AboutForm.

--- AresPro.WinFormsUi/Views/AboutForm.Designer.cs
+++ AresPro.WinFormsUi/Views/AboutForm.Designer.cs
@@ -28,104 +28,123 @@ partial class AboutForm
     /// </summary>
     private void InitializeComponent()
     {
-        this.OkCommand = new System.Windows.Forms.Button();
-        this.IconPicture = new System.Windows.Forms.PictureBox();
-        this.CopyrightLabel = new System.Windows.Forms.Label();
-        this.VersionLabel = new System.Windows.Forms.Label();
-        this.TitleLabel = new System.Windows.Forms.Label();
-        this.AboutFrame = new System.Windows.Forms.GroupBox();
-        this.CompanyLabel = new System.Windows.Forms.Label();
-        ((System.ComponentModel.ISupportInitialize)(this.IconPicture)).BeginInit();
-        this.AboutFrame.SuspendLayout();
-        this.SuspendLayout();
+        OkButton = new Button();
+        IconPictureBox = new PictureBox();
+        CopyrightLabel = new Label();
+        VersionLabel = new Label();
+        TitleLabel = new Label();
+        AboutFrame = new GroupBox();
+        CompanyLabel = new Label();
+        ((System.ComponentModel.ISupportInitialize)IconPictureBox).BeginInit();
+        AboutFrame.SuspendLayout();
+        SuspendLayout();
         // 
-        // OkCommand
+        // OkButton
         // 
-        this.OkCommand.DialogResult = System.Windows.Forms.DialogResult.OK;
-        this.OkCommand.Location = new System.Drawing.Point(444, 267);
-        this.OkCommand.Name = "OkCommand";
-        this.OkCommand.Size = new System.Drawing.Size(75, 23);
-        this.OkCommand.TabIndex = 4;
-        this.OkCommand.Text = "Ok";
-        this.OkCommand.UseVisualStyleBackColor = true;
+        OkButton.DialogResult = DialogResult.OK;
+        OkButton.Location = new Point(203, 133);
+        OkButton.Margin = new Padding(4, 3, 4, 3);
+        OkButton.Name = "OkButton";
+        OkButton.Size = new Size(88, 27);
+        OkButton.TabIndex = 4;
+        OkButton.Text = "Ok";
+        OkButton.UseVisualStyleBackColor = true;
+        OkButton.Click += OkButton_Click;
         // 
-        // IconPicture
+        // IconPictureBox
         // 
-        this.IconPicture.Location = new System.Drawing.Point(281, 170);
-        this.IconPicture.Name = "IconPicture";
-        this.IconPicture.Size = new System.Drawing.Size(32, 32);
-        this.IconPicture.TabIndex = 3;
-        this.IconPicture.TabStop = false;
+        IconPictureBox.Location = new Point(13, 30);
+        IconPictureBox.Margin = new Padding(4, 3, 4, 3);
+        IconPictureBox.Name = "IconPictureBox";
+        IconPictureBox.Size = new Size(37, 37);
+        IconPictureBox.TabIndex = 3;
+        IconPictureBox.TabStop = false;
         // 
         // CopyrightLabel
         // 
-        this.CopyrightLabel.AutoSize = true;
-        this.CopyrightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-        this.CopyrightLabel.Location = new System.Drawing.Point(6, 58);
-        this.CopyrightLabel.Name = "CopyrightLabel";
-        this.CopyrightLabel.Size = new System.Drawing.Size(0, 16);
-        this.CopyrightLabel.TabIndex = 2;
+        CopyrightLabel.AutoSize = true;
+        CopyrightLabel.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+        CopyrightLabel.Location = new Point(7, 67);
+        CopyrightLabel.Margin = new Padding(4, 0, 4, 0);
+        CopyrightLabel.Name = "CopyrightLabel";
+        CopyrightLabel.Size = new Size(131, 16);
+        CopyrightLabel.TabIndex = 2;
+        CopyrightLabel.Text = "Copyright (c) 2025";
         // 
         // VersionLabel
         // 
-        this.VersionLabel.AutoSize = true;
-        this.VersionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-        this.VersionLabel.Location = new System.Drawing.Point(6, 37);
-        this.VersionLabel.Name = "VersionLabel";
-        this.VersionLabel.Size = new System.Drawing.Size(0, 16);
-        this.VersionLabel.TabIndex = 1;
+        VersionLabel.AutoSize = true;
+        VersionLabel.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+        VersionLabel.Location = new Point(7, 43);
+        VersionLabel.Margin = new Padding(4, 0, 4, 0);
+        VersionLabel.Name = "VersionLabel";
+        VersionLabel.Size = new Size(96, 16);
+        VersionLabel.TabIndex = 1;
+        VersionLabel.Text = "Version 1.0.0";
         // 
         // TitleLabel
         // 
-        this.TitleLabel.AutoSize = true;
-        this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-        this.TitleLabel.Location = new System.Drawing.Point(6, 16);
-        this.TitleLabel.Name = "TitleLabel";
-        this.TitleLabel.Size = new System.Drawing.Size(0, 16);
-        this.TitleLabel.TabIndex = 0;
+        TitleLabel.AutoSize = true;
+        TitleLabel.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+        TitleLabel.Location = new Point(7, 18);
+        TitleLabel.Margin = new Padding(4, 0, 4, 0);
+        TitleLabel.Name = "TitleLabel";
+        TitleLabel.Size = new Size(67, 16);
+        TitleLabel.TabIndex = 0;
+        TitleLabel.Text = "Ares Pro";
         // 
         // AboutFrame
         // 
-        this.AboutFrame.Controls.Add(this.CompanyLabel);
-        this.AboutFrame.Controls.Add(this.CopyrightLabel);
-        this.AboutFrame.Controls.Add(this.VersionLabel);
-        this.AboutFrame.Controls.Add(this.TitleLabel);
-        this.AboutFrame.Location = new System.Drawing.Point(319, 161);
-        this.AboutFrame.Name = "AboutFrame";
-        this.AboutFrame.Size = new System.Drawing.Size(200, 100);
-        this.AboutFrame.TabIndex = 2;
-        this.AboutFrame.TabStop = false;
+        AboutFrame.Controls.Add(CompanyLabel);
+        AboutFrame.Controls.Add(CopyrightLabel);
+        AboutFrame.Controls.Add(VersionLabel);
+        AboutFrame.Controls.Add(TitleLabel);
+        AboutFrame.Location = new Point(58, 12);
+        AboutFrame.Margin = new Padding(4, 3, 4, 3);
+        AboutFrame.Name = "AboutFrame";
+        AboutFrame.Padding = new Padding(4, 3, 4, 3);
+        AboutFrame.Size = new Size(233, 115);
+        AboutFrame.TabIndex = 2;
+        AboutFrame.TabStop = false;
         // 
         // CompanyLabel
         // 
-        this.CompanyLabel.AutoSize = true;
-        this.CompanyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-        this.CompanyLabel.Location = new System.Drawing.Point(6, 79);
-        this.CompanyLabel.Name = "CompanyLabel";
-        this.CompanyLabel.Size = new System.Drawing.Size(0, 16);
-        this.CompanyLabel.TabIndex = 3;
+        CompanyLabel.AutoSize = true;
+        CompanyLabel.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+        CompanyLabel.Location = new Point(7, 91);
+        CompanyLabel.Margin = new Padding(4, 0, 4, 0);
+        CompanyLabel.Name = "CompanyLabel";
+        CompanyLabel.Size = new Size(94, 16);
+        CompanyLabel.TabIndex = 3;
+        CompanyLabel.Text = "Geeksoft.net";
         // 
         // AboutForm
         // 
-        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Controls.Add(this.OkCommand);
-        this.Controls.Add(this.IconPicture);
-        this.Controls.Add(this.AboutFrame);
-        this.Name = "AboutForm";
-        this.Text = "AboutForm";
-        ((System.ComponentModel.ISupportInitialize)(this.IconPicture)).EndInit();
-        this.AboutFrame.ResumeLayout(false);
-        this.AboutFrame.PerformLayout();
-        this.ResumeLayout(false);
-
+        AcceptButton = OkButton;
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(304, 172);
+        Controls.Add(OkButton);
+        Controls.Add(IconPictureBox);
+        Controls.Add(AboutFrame);
+        FormBorderStyle = FormBorderStyle.FixedDialog;
+        Margin = new Padding(4, 3, 4, 3);
+        MaximizeBox = false;
+        MinimizeBox = false;
+        Name = "AboutForm";
+        ShowIcon = false;
+        ShowInTaskbar = false;
+        Text = "About...";
+        ((System.ComponentModel.ISupportInitialize)IconPictureBox).EndInit();
+        AboutFrame.ResumeLayout(false);
+        AboutFrame.PerformLayout();
+        ResumeLayout(false);
     }
 
     #endregion
 
-    internal System.Windows.Forms.Button OkCommand;
-    internal System.Windows.Forms.PictureBox IconPicture;
+    internal System.Windows.Forms.Button OkButton;
+    internal System.Windows.Forms.PictureBox IconPictureBox;
     internal System.Windows.Forms.Label CopyrightLabel;
     internal System.Windows.Forms.Label VersionLabel;
     internal System.Windows.Forms.Label TitleLabel;
--- AresPro.WinFormsUi/Views/AboutForm.cs
+++ AresPro.WinFormsUi/Views/AboutForm.cs
@@ -5,5 +5,11 @@ public partial class AboutForm : Form
     public AboutForm()
     {
         InitializeComponent();
+        IconPictureBox.Image = Properties.Resources.a_pro.ToBitmap();
+    }
+
+    private void OkButton_Click(object sender, EventArgs e)
+    {
+        Close();
     }
 }
